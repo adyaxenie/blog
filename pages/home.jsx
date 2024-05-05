ceffa8bfef067369b97f53a682e3fd93a4a5c77c
adyaxenie <adyaxenie@gmail.com>
2024-05-04 22:50:43 -0700

    scroll to sections, projects data, formating

--- pages/home.jsx
+++ pages/home.jsx
@@ -6,20 +6,19 @@ import Projects from '../components/projects';
 import Landing from '../components/landing';
 
 export default function Index() {
-    return (
-        <>
-            <Head>
-                <title>My Blog</title>
-                <meta name="description" content="Welcome to My Blog where I share my thoughts and ideas." />
-            </Head>
-            <Navbar />
-            <Landing/>
-            <div className='h-screen'>
-                <Home />
-            </div>
-            <div className='h-screen'>
-                <Projects />
-            </div>
-        </>
-    );
+  return (
+    <>
+      <Head>
+        <title>My Blog</title>
+        <meta name="description" content="Welcome to My Blog where I share my thoughts and ideas." />
+      </Head>
+      <Navbar />
+      <Landing id="home" />
+      <div id="projects" className='h-screen'>
+        <Home />
+      </div>
+      <div id="experience" className='h-screen'>
+      </div>
+    </>
+  );
 }
