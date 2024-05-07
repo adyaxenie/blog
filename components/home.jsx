1fd5f6beaa9979f678828e633aade5b35d8731d6
adyaxenie <adyaxenie@gmail.com>
2024-05-06 21:04:21 -0700

    timeline, about, footer, formatting

--- components/home.jsx
+++ components/home.jsx
@@ -5,8 +5,10 @@ import Projects from './projects';
 const Home = () => {
     return (
         <div className="justify-center items-center py-20 p-10">
-            <h1 className="divider text-6xl font-medium">Projects</h1>
-            <Projects />
+            <h1 className="divider text-6xl font-medium w-1/2 md:mx-auto">Projects</h1>
+            <div className="mockup-window border bg-base-300 mt-20">
+                <Projects />
+            </div>
         </div>
     );
 };
---
+++ components/timeline.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+
+const Timeline = () => {
+    return (
+        <div className="justify-center items-center p-10">
+            <h1 className="divider text-6xl font-medium w-1/3 md:mx-auto py-10">Experience</h1>
+            
+            <ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical p-5">
+            <li>
+                    <hr />
+                    <div className="timeline-middle">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+                    </div>
+                    <div className="timeline-end mb-10">
+                        <time className="font-mono italic">May 2023 - Present</time>
+                        <div className="text-lg font-black">Product Manager at Voter.Vote</div>
+                        <ul>
+                            <li>Full stack development of website applications.</li>
+                            <li>Backend architecture to frontend design and deployment (React, Django, Heroku, AWS).</li>
+                            <li>Work cross-functionally with team to strategize, build, and deliver product features.</li>
+                        </ul>
+                    </div>
+                    <hr />
+                </li>
+                <li>
+                    <hr />
+                    <div className="timeline-middle">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+                    </div>
+                    <div className="timeline-start md:text-end mb-10">
+                        <time className="font-mono italic">August 2019 - May 2023</time>
+                        <div className="text-lg font-black">San Jose State University</div>
+                        <div className="subheading mb-3">Bachelor of Business Administration</div>
+                        <p>Concentration in Entrepreneurship</p>
+                        <p>Dean's Scholar (2022), Member of Ideas Entrepreneurial Club</p>
+                    </div>
+                    <hr />
+                </li>
+                <li>
+                    <hr />
+                    <div className="timeline-middle">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+                    </div>
+                    <div className="timeline-end mb-10">
+                        <time className="font-mono italic">August 2015 - May 2019</time>
+                        <div className="text-lg font-black">Leigh High School</div>
+                        <div className="subheading mb-3">Project Lead The Way Engineering</div>
+                        <p>4 Year Scholar in Engineering - Capstone Projects</p>
+                        <p>Learned basics of mechanical, computer and electrical engineering through PLTW program.</p>
+                    </div>
+                    <hr />
+                </li>
+            </ul>
+        </div>
+)}
+
+export default Timeline;
