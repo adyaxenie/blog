1fb480aaad96f66780d578e22690cdf0f88ed326
adyaxenie <adyaxenie@gmail.com>
2024-05-09 21:34:39 -0700

    blog pages and apis

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import "daisyui/dist/full.css";
 
 import Home from '../pages/home';
+import Blog from '../pages/blog';
 
 export default function Index() {
     return <Home />;
