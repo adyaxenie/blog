71a30e833f80526522640470af535d788f3d753c
adyaxenie <adyaxenie@gmail.com>
2024-07-06 00:02:05 -0700

    run build fixes

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import Image from "next/image";
 import "daisyui/dist/full.css";
 
 import Home from '../pages/home';
-import Blog from '../pages/blog';
 
 export default function Index() {
     return <Home />
