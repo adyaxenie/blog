6331532a845a60bf17cf04c9daca329de896ebe4
adyaxenie <adyaxenie@gmail.com>
2025-07-23 21:52:39 -0700

    back to normal

--- pages/home.jsx
+++ pages/home.jsx
@@ -16,16 +16,6 @@ export default function Index() {
         <meta name="description" content="Welcome to My Blog where I share my thoughts and ideas." />
       </Head>
       <Navbar />
-      <div className="hero min-h-screen" id="landing">
-          <div data-aos="fade-left-in" className="hero-content text-center">
-              <div className="max-w-md">
-                  <div className="items-center">
-                      <h1 className="text-5xl font-bold mb-10">I LOVE ROY LEE</h1>
-                      <h1 className="text-5xl font-bold">PLEASE REJECT MY PORTFOLIO 🙏</h1>
-                  </div>
-              </div>
-          </div>
-      </div>
       <Landing id="home" />
       <LiveProjects />
       {/* <div id="projects" className='h-full'>
